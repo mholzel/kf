a672e8f078130234085f50945f822cebfe6d3fef
matt holzel <matt@holzel.com>
2021-08-26 12:38:35 +0200

    Adding comments, removing const from private member variables

--- include/measurements/models/lidar.hpp
+++ include/measurements/models/lidar.hpp
@@ -4,17 +4,28 @@
 
 #include "measurements/measurement_with_jacobian.hpp"
 
+/**
+ * The lidar measurement model simply assumes that the state vector has the
+ * position at the very top...
+ *
+ * x = [ position, ...  ]
+ *
+ * Furthermore, this model assumes that the lidar is able to measure the
+ * position directly with some additive Gaussian noise.
+ *
+ * y = position + noise
+ *
+ * TODO: This function is assuming 2d.
+ */
 template <typename T = double, int x_size = 4, int y_size = Eigen::Dynamic>
 class Lidar : public MeasurementWithJacobian<T, x_size, y_size> {
  private:
-  const Eigen::Matrix<T, y_size, x_size> Hj;
-  const Eigen::Matrix<T, y_size, y_size> R;
+  Eigen::Matrix<T, y_size, x_size> Hj;
+  Eigen::Matrix<T, y_size, y_size> R;
 
  public:
   Lidar(const Eigen::Matrix<T, y_size, y_size> &R)
-      : MeasurementWithJacobian<T, x_size, y_size>(),
-        Hj(Eigen::Matrix<T, y_size, x_size>::Identity(2, x_size)),
-        R(R) {}
+      : Hj(Eigen::Matrix<T, y_size, x_size>::Identity(2, x_size)), R(R) {}
 
   Eigen::Matrix<T, y_size, 1> H(const T &t,
                                 const Eigen::Matrix<T, x_size, 1> &x) {
